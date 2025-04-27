da60a60f6c8b6dc2dc6d9de78bd545fea66120cc
Rahul Krishna <i.m.ralk@gmail.com>
2025-04-26 20:49:30 -0400

    Add API to get a specific class in the application.
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>

--- test/unit/analysis/java/JavaAnalysis.test.ts
+++ test/unit/analysis/java/JavaAnalysis.test.ts
@@ -17,13 +17,22 @@ test("Must get Symbol Table", async () => {
 });
 
 test("Must get all classes in a Java application", async () => {
-  expect(await daytraderJavaAnalysis.getAllClasses()).toBeDefined();
+  await expect(daytraderJavaAnalysis.getAllClasses()).toBeDefined();
 });
 
 test("Must get a specific class the application", async () => {
   const tradeDirectObject = await daytraderJavaAnalysis.getClassByQualifiedName("com.ibm.websphere.samples.daytrader.impl.direct.TradeDirect");
-  console.log(tradeDirectObject);
   expect(async () => JType.parse(tradeDirectObject)).not.toThrow(); 
+});
+
+test("Must throw error when a requested class in the application does not exist", async () => {
+  /**
+   * Quick note to self: There is a subtle difference between await expect(...) and expect(await ...)
+   * When there is an error, the reject happens even before the expect can be honored. So instead, we await the expect
+   * by saying "Hey, I expect this promise to be rejected with this error ..." 
+   */
+  await expect(daytraderJavaAnalysis.getClassByQualifiedName("this.class.does.not.Exist")).rejects.toThrow(
+    "Class this.class.does.not.Exist not found in the application."); 
 });
 
 test("Must get all methods in the application", () => {
